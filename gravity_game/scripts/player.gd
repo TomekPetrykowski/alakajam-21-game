b3a2dfb895ac41e1908fb448e509b4ee3dfe99e1
Paweł Żabczyński <p_k_zab@wp.pl>
2025-02-22 19:26:28 +0100

    polished movement and added animation

--- gravity_game/scripts/player.gd
+++ gravity_game/scripts/player.gd
@@ -28,7 +28,7 @@ var pointing_to = Vector2(-1,0)
 func _physics_process(delta: float) -> void:
 	if rotating_around != null:
 		rotation_point = rotating_around.position
-	pointing_to = Vector2(-1,0).rotated(rotation)
+	pointing_to = Vector2(-1,0).rotated($Sprite2D.rotation)
 	if rotation_point != Vector2.ZERO:
 		up_direction = vector2planet*-1
 		vector_rotation = position.angle_to_point(rotation_point)
@@ -36,8 +36,9 @@ func _physics_process(delta: float) -> void:
 		
 		if stable:
 			planet_movement.y += 10
-			rotation = vector_rotation
+			$Sprite2D.rotation = vector_rotation
 		else:
+			
 			jetpack_movement += 10 * vector2planet
 	jetpack_movement*=0.99
 	
@@ -45,6 +46,7 @@ func _physics_process(delta: float) -> void:
 		stable = true
 		jetpack_movement = Vector2.ZERO
 		self.fuel += delta*2
+		planet_movement.y = 0
 	if Input.is_action_just_pressed("ui_accept"):
 		if is_on_floor():
 			planet_movement.y = JUMP_VELOCITY
@@ -53,28 +55,42 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_just_released("ui_accept") and jetpacking:
 		if rotation_point != Vector2.ZERO:
 			stable = true
+		else:
+			$AnimationPlayer.play("floating")
 		jetpacking = false
 	
 	if jetpacking:
+		$AnimationPlayer.play("flying")
 		if fuel <=0.0:
 			jetpacking = false
+			
 		else:
 			##fuel -= delta
 			if stable:
 				planet_movement.y -= 15
+				#if movement.y > -500:
+					#movement.y = -500
 			else:
-				jetpack_movement +=11*pointing_to
-			if movement.y < -300:
-				movement.y = -300
+				jetpack_movement +=15*pointing_to
+				#jetpack_movement
+				#if jetpack_movement.length()>=500:
+					
+			
 	
 	var direction := Input.get_axis("left", "right")
 	if rotation_point == Vector2.ZERO or not stable:
 		planet_movement.x = 0
 		vector_rotation += 0.1*direction
-		rotation += 0.1*direction
+		$Sprite2D.rotation += 0.1*direction
 	elif direction:
+		if direction <0:
+			$Sprite2D.flip_v = true
+		else:
+			$Sprite2D.flip_v = false
+		$AnimationPlayer.play("walking")
 		planet_movement.x = direction * SPEED
 	else:
+		$AnimationPlayer.play("idle")
 		planet_movement.x = 0
 	if rotation_point != Vector2.ZERO:
 		#velocity = vector2planet * vertical
@@ -83,6 +99,19 @@ func _physics_process(delta: float) -> void:
 		velocity = Vector2.ZERO
 	velocity += jetpack_movement
 	move_and_slide()
+	
+	#print(rad_to_deg($Sprite2D.rotation))
+	#print(rad_to_deg(get_local_mouse_position().angle()))
+	#print(rad_to_deg($Sprite2D.rotation-get_local_mouse_position().angle()))
+	#if get_local_mouse_position().rotated($Sprite2D.rotation).x<0:
+		#$Sprite2D.flip_v = false
+		#$Weapon.flip_v = false
+	#else:
+		#$Sprite2D.flip_v = true
+		#$Weapon.flip_v = true
+	$Weapon.rotation=get_angle_to(get_global_mouse_position())
+	$Weapon.position = (get_local_mouse_position().normalized())*6*4
+	#print(velocity,planet_movement,jetpack_movement)
 	
 
 func _change_fuel(new):
@@ -99,9 +128,11 @@ func _changed_rotating_around(new):
 	if new == null:
 		rotation_point = Vector2.ZERO
 		stable = false
-		jetpack_movement = planet_movement.x * vector2planet.rotated(deg_to_rad(-90)) + planet_movement.y * vector2planet
+		jetpack_movement += planet_movement.x * vector2planet.rotated(deg_to_rad(-90)) + planet_movement.y * vector2planet
+		planet_movement = Vector2.ZERO
 		planet_pull = 0
 		motion_mode = MOTION_MODE_FLOATING
+		$AnimationPlayer.play("floating")
 	else:
 		rotation_point=new.position
 		vector2planet = (rotation_point - position).normalized()
@@ -110,4 +141,5 @@ func _changed_rotating_around(new):
 		planet_pull = 10
 		if not jetpacking:
 			stable = true
+			$AnimationPlayer.play("idle")
 		motion_mode = MOTION_MODE_GROUNDED
