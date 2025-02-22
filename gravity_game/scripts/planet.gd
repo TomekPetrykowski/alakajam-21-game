cf93f8a69dbafe8cabab66617e05c820999fa695
Paweł Żabczyński <p_k_zab@wp.pl>
2025-02-22 14:32:05 +0100

    added more advanced player movement

--- gravity_game/scripts/planet.gd
+++ gravity_game/scripts/planet.gd
@@ -3,9 +3,13 @@ extends AnimatableBody2D
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.name == "Player":
-		body.rotation_point = position
+		body.rotating_around = self
 
 
-#func _on_area_2d_body_exited(body: Node2D) -> void:
-	#if body.name == "Player":
-		#body.rotation_point = position
+func _on_area_2d_body_exited(body: Node2D) -> void:
+	if body.name == "Player" and body.rotation_point == position:
+		body.rotating_around = null
+
+
+#func _physics_process(delta: float) -> void:
+	#position.x+=1
--- gravity_game/scripts/player.gd
+++ gravity_game/scripts/player.gd
@@ -7,80 +7,107 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -300.0
 
+var stable = false
 var vector2planet = Vector2.ZERO
-var gravity = Vector2.ZERO
 var jump_power = 0
 var vertical = 0
 var horizontal = 0
+var jetpack_movement = Vector2.ZERO
+var planet_movement = Vector2.ZERO
+var movement = Vector2.ZERO
+var jetpacking = false
 
+var vector_rotation = 0
 var rotation_point = Vector2.ZERO
-
 var fuel = 3.0 :set = _change_fuel
+var prev_vector = Vector2.ZERO
+var planet_pull = 0
+var rotating_around = null :set = _changed_rotating_around
+var pointing_to = Vector2(-1,0)
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	
-	#velocity = gravity
-	#velocity = Vector2.ZERO
-	#if not is_on_floor():
-		#velocity += get_gravity() * delta
-	#position = get_global_mouse_position()
-	look_at(rotation_point)
-	#print(rotation)
-	
-	vector2planet = (rotation_point - position).normalized()
-	#print(vector2planet.normalized())
-	vertical += 10
-	up_direction = vector2planet*-1
-	
-	
-	#if is_on_floor():
-		#gravity = Vector2.ZERO
-		#print("yay")
-	#else:
-		#print("nay")
-		#gravity += 5 * vector2planet
-	#velocity = gravity
-	#if is_on_ceiling():
-		#print("aya")
-	#if is_on_wall():
-		#print("aaa")
-	#rotation = rad_to_deg(get_angle_to(planet.position))s
-	#print(rad_to_deg(get_angle_to(planet.position)))
-#
-	## Handle jump.
-	if is_on_floor():
-		self.fuel += delta
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		vertical = JUMP_VELOCITY
-	elif Input.is_action_pressed("ui_accept") and not is_on_floor() and fuel >= 0.0:
-		fuel -= delta
-		vertical -= 15
-		if vertical < -300:
-			vertical = -300
-	
-	
-	#velocity = vertical * vector2planet
-#
-	## Get the input direction and handle the movement/deceleration.
-	## As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("left", "right")
-	if direction:
-		#velocity = direction * SPEED * vector2planet.rotated(deg_to_rad(90))
-		horizontal = direction * SPEED
-	else:
-		horizontal = 0
-		#velocity.x = move_toward(velocity.x, 0, SPEED)
+	if rotating_around != null:
+		rotation_point = rotating_around.position
+	pointing_to = Vector2(-1,0).rotated(rotation)
+	if rotation_point != Vector2.ZERO:
+		up_direction = vector2planet*-1
+		vector_rotation = position.angle_to_point(rotation_point)
+		vector2planet = (rotation_point - position).normalized()
 		
-	velocity = horizontal * vector2planet.rotated(deg_to_rad(-90)) + vertical * vector2planet
+		if stable:
+			planet_movement.y += 10
+			rotation = vector_rotation
+		else:
+			jetpack_movement += 10 * vector2planet
+	jetpack_movement*=0.99
+	
+	if is_on_floor():
+		stable = true
+		jetpack_movement = Vector2.ZERO
+		self.fuel += delta*2
+	if Input.is_action_just_pressed("ui_accept"):
+		if is_on_floor():
+			planet_movement.y = JUMP_VELOCITY
+		elif fuel >= 0.0:
+			jetpacking = true
+	if Input.is_action_just_released("ui_accept") and jetpacking:
+		if rotation_point != Vector2.ZERO:
+			stable = true
+		jetpacking = false
+	
+	if jetpacking:
+		if fuel <=0.0:
+			jetpacking = false
+		else:
+			##fuel -= delta
+			if stable:
+				planet_movement.y -= 15
+			else:
+				jetpack_movement +=11*pointing_to
+			if movement.y < -300:
+				movement.y = -300
+	
+	var direction := Input.get_axis("left", "right")
+	if rotation_point == Vector2.ZERO or not stable:
+		planet_movement.x = 0
+		vector_rotation += 0.1*direction
+		rotation += 0.1*direction
+	elif direction:
+		planet_movement.x = direction * SPEED
+	else:
+		planet_movement.x = 0
+	if rotation_point != Vector2.ZERO:
+		#velocity = vector2planet * vertical
+		velocity = planet_movement.x * vector2planet.rotated(deg_to_rad(-90)) + planet_movement.y * vector2planet
+	else:
+		velocity = Vector2.ZERO
+	velocity += jetpack_movement
 	move_and_slide()
 	
 
 func _change_fuel(new):
 	if new < 0.0:
 		fuel = 0.0
-	if new > 3.0:
-		fuel = 3.0
+	elif new > 10.0:
+		fuel = 10.0
 	else:
 		fuel = new
-	fuel_label.text = str(fuel)+"s"
+	fuel_label.text = str(snapped(fuel,0.01))+"s"
+
+func _changed_rotating_around(new):
+	rotating_around = new
+	if new == null:
+		rotation_point = Vector2.ZERO
+		stable = false
+		jetpack_movement = planet_movement.x * vector2planet.rotated(deg_to_rad(-90)) + planet_movement.y * vector2planet
+		planet_pull = 0
+		motion_mode = MOTION_MODE_FLOATING
+	else:
+		rotation_point=new.position
+		vector2planet = (rotation_point - position).normalized()
+		planet_movement = planet_movement.y * vector2planet
+		prev_vector = movement.x * vector2planet.rotated(deg_to_rad(-90)) + movement.y * vector2planet
+		planet_pull = 10
+		if not jetpacking:
+			stable = true
+		motion_mode = MOTION_MODE_GROUNDED
